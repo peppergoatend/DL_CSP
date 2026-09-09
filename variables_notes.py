0d40c448ad7a92f722b2840cc53955011ab1aa92
peppergoatend <dara.le@ucas-edu.net>
2026-09-09 13:52:11 -0600

    variable notes

---
+++ variables_notes.py
@@ -0,0 +1,7 @@
+# DL, Variables notes
+
+name = input("Tell me your name:")
+
+print("CSP is the best class!")
+print("it is taught by the coolest teacher!")
+print("Hello", name, "welcome to class!")
