42ca74ac4ec95465c544e921db1d8ccd59ee20d8
peppergoatend <dara.le@ucas-edu.net>
2026-09-10 13:31:59 -0600

    variables_practice

---
+++ variables_practice.py
@@ -0,0 +1,16 @@
+# DL, Variables Practice
+
+name = "Dara"
+ten_number = "1"
+hundred_number = "24"
+breakfast = "Yogurt"
+color = "Navy"
+school_name = "UCAS"
+year = "2026"
+eye_color = "brown"
+age = "53"
+subject = "English"
+
+print(name, ten_number, hundred_number, breakfast, color, school_name, year, eye_color, age, subject)
+
+# If my favorite subject was Art here, this list would have reminded you of someone very specific in this school.
