39780f64847bd9a8598b449b7c5d2ae07614064f
peppergoatend <dara.le@ucas-edu.net>
2026-09-11 13:47:44 -0600

    Physical and Logical Memory notes

---
+++ physical_logical_memory- notes.py
@@ -0,0 +1,8 @@
+# Dl, Physical and Logical Memory notes
+
+pi = 3.1415
+
+everything = 42
+
+print(f"The variable pi is {pi} and it is saved at {id(pi)}")
+print(f"The variable everything is {everything} and is saved at {id(everything)}")
