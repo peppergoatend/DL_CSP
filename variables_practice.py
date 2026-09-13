8cd1aee4d1de40620b9085063ea0d0a798883f6c
peppergoatend <dara.le@ucas-edu.net>
2026-09-12 22:01:55 -0600

    Remove commented-out code in variables_practice.py

--- variables_practice.py
+++ variables_practice.py
@@ -12,5 +12,3 @@ age = "53"
 subject = "English"
 
 print(name, ten_number, hundred_number, breakfast, color, school_name, year, eye_color, age, subject)
-
-# If my favorite subject was Art here, this list would have reminded you of someone very specific in this school.
