e5e2d725ab2265a531ef9f68de5ae7a88131aa2d
peppergoatend <dara.le@ucas-edu.net>
2026-09-14 13:30:06 -0600

    memory_diagram_activity

---
+++ memory_diagram_activity.py
@@ -0,0 +1,18 @@
+name = "Alex"
+age = 14
+score = 10
+score = 25
+height = 5.9
+city = "Denver"
+zip_code = 80202
+price1 = 10
+price2 = 10
+
+print(f"The variable name is {name} and it is saved at {id(name)}")
+print(f"The variable age is {age} and it is saved at {id(age)}")
+print(f"The variable score is {score} and it is saved at {id(score)}")
+print(f"The variable height is {height} and it is saved at {id(height)}")
+print(f"The variable city is {city} and it is saved at {id(city)}")
+print(f"The variable zip_code is {zip_code} and it is saved at {id(zip_code)}")
+print(f"The variable price1 is {price1} and it is saved at {id(price1)}")
+print(f"The variable price2 is {price2} and it is saved at {id(price2)}")
