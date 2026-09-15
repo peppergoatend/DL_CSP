56dad63adfb68a81454363032ac62ea3b75ab924
peppergoatend <dara.le@ucas-edu.net>
2026-09-15 14:08:44 -0600

    Update name input to use title case formatting
    Change name formatting to use title case.

--- hello_user.py
+++ hello_user.py
@@ -1,10 +1,8 @@
 # DL, Physical & Logical Memory
 while True:
-        name = input("Tell me your name:").strip().capitalize()
+        name = input("Tell me your name:").strip().title()
         if name.isnumeric():
             print("Sorry, that is a number. Please enter your name.")
-        elif " " in name:
-            print("I said your name!")
         else:
             break
 
