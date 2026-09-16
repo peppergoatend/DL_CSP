9c302d11328828506708b859156825cd21da46ec
peppergoatend <dara.le@ucas-edu.net>
2026-09-16 13:52:32 -0600

    Intergers and Expressions notes

---
+++ Notes/intergers_expressions_notes.py
@@ -0,0 +1,28 @@
+# DL, Intergers and Expressions Notes
+
+# Intergers (whole numbers)
+people = 18
+
+shoes = people * 2
+
+hand = people * 2
+
+# Float (decimals)
+pi = 3.1415
+
+water = 60.5
+
+temp = 68.5
+
+price = 1.99
+
+# Expressions
+print(water*price)
+print(f"Your {water} gallons of water costs ${round(water*price, 2)}")
+
+# modulo and integer division
+print(f"18%4= {18/4} which is {18//4} with a remainder of {18%4}")
+print(f"18%5= {18/5} which is {18//5} with a remainder of {18%5}")
+print(f"18%6= {18/6} which is {18//6} with a remainder of {18%6}")
+
+print(int(people*2*(5-1)/3))
