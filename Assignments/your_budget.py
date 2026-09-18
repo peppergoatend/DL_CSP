e857f96d3045e07eb556159d831cafcadb27a5a2
peppergoatend <dara.le@ucas-edu.net>
2026-09-18 02:15:59 +0000

    Your Budget

--- Assignments/your_budget.py
+++ Assignments/your_budget.py
@@ -9,10 +9,10 @@ groceries = float(input("What is your monthly groceries:"))
 
 transportation = float(input("What is your monthly transportation:"))
 
-print(f"Your rent is ${round (rent)} and that is {round(rent/income*100)} % of your income.")
-print(f"Your utilities are ${round(utilities)} and that is {round(utilities/income*100)} % of your income.")
-print(f"Your groceries are ${round(groceries)} and that is {round(groceries/income*100)} % of your income.")
-print(f"Your transportation costs are ${round(transportation)} and that is {round(transportation/income*100)} % of your income.")
+print(f"Your rent is ${round (rent, 2)} and that is {round(rent/income*100)} % of your income.")
+print(f"Your utilities are ${round(utilities, 2)} and that is {round(utilities/income*100)} % of your income.")
+print(f"Your groceries are ${round(groceries, 2)} and that is {round(groceries/income*100)} % of your income.")
+print(f"Your transportation costs are ${round(transportation, 2)} and that is {round(transportation/income*100)} % of your income.")
 
-print(f"You should save ${round(income/10)} a month, that is 10% of your income.")
-print(f"You have ${round(income - (rent + utilities + groceries + transportation) - income/10)} of spending money each month!")
+print(f"You should save ${round(income/10, 2)} a month, that is 10% of your income.")
+print(f"You have ${round(income - (rent + utilities + groceries + transportation) - income/10, 2)} of spending money each month!")
